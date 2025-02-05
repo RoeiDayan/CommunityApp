48db3e4f760c683edb3806629f29971ce5d0a1a4
User <User@LAB3-3>
2025-02-05 13:14:52 +0200

    worked on profile

--- CommunityApp/ViewModels/ProfilePageViewModel.cs
+++ CommunityApp/ViewModels/ProfilePageViewModel.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 
 namespace CommunityApp.ViewModels
 {
-    public class ProfilePageViewModel:ViewModelBase
+    public class ProfilePageViewModel : ViewModelBase
     {
         private CommunityWebAPIProxy proxy;
         private IServiceProvider serviceProvider;
@@ -20,7 +20,31 @@ namespace CommunityApp.ViewModels
             this.serviceProvider = serviceProvider;
             this.proxy = proxy;
             CurMember = ((App)Application.Current).CurMem;
+            MemberName = ((App)Application.Current).LoggedInUser.Name;
+            MemberRole = CurMember.Role;
+            Balance = CurMember.Balance;
+            UnitNum = CurMember.UnitNum;
+            IsLiable = CurMember.IsLiable;
+            IsResident = CurMember.IsResident;
+            ChooseRole();
         }
+
+        public void ChooseRole()
+        {
+            if (IsManager.HasValue && IsManager.Value)
+            {
+                MemberRole = "Manager";
+            }
+            else if (IsProvider.HasValue && IsProvider.Value)
+            {
+                MemberRole = "Provider";
+            }
+            else
+            {
+                MemberRole = "Resident";
+            }
+        }
+        private string memberName;
         private Member? curMember;
         public Member? CurMember
         {
@@ -30,6 +54,99 @@ namespace CommunityApp.ViewModels
                 curMember = value;
                 OnPropertyChanged("CurMember");
              
+            }
+        }
+        public string MemberName
+        {
+            get { return memberName; }
+            set
+            {
+                memberName = value;
+                OnPropertyChanged("MemberName");
+            }
+        }
+        private string? memberRole;
+
+        public int? balance;
+
+        public int? unitNum;
+
+        public bool? isLiable;
+
+        public bool? isResident;
+
+        public bool? isManager;
+
+        public bool? isProvider;
+
+        public string? MemberRole
+        {
+            get => memberRole;
+            set
+            {
+                memberRole = value;
+                OnPropertyChanged("MemberRole");
+
+            }
+        }
+        public int? Balance
+        {
+            get => balance;
+            set
+            {
+                balance = value;
+                OnPropertyChanged("Balance");
+
+            }
+        }
+        public int? UnitNum
+        {
+            get => unitNum;
+            set
+            {
+                unitNum = value;
+                OnPropertyChanged("UnitNum");
+
+            }
+        }
+        public bool? IsLiable
+        {
+            get => isLiable;
+            set
+            {
+                isLiable = value;
+                OnPropertyChanged("IsLiable");
+
+            }
+        }
+        public bool? IsResident
+        {
+            get => isResident;
+            set
+            {
+                isResident = value;
+                OnPropertyChanged("IsResident");
+
+            }
+        }
+        public bool? IsManager
+        {
+            get => isManager;
+            set
+            {
+                isManager = value;
+                OnPropertyChanged("IsManager");
+
+            }
+        }
+        public bool? IsProvider
+        {
+            get => isProvider;
+            set
+            {
+                isProvider = value;
+                OnPropertyChanged("IsProvider");
+
             }
         }
     }
