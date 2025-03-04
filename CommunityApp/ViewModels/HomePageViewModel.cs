590caa323aa0a7e74aca801975e16729cb36bdc1
User <User@LAB3-3>
2025-03-04 11:18:18 +0200

    Worked on homepage

--- CommunityApp/ViewModels/HomePageViewModel.cs
+++ CommunityApp/ViewModels/HomePageViewModel.cs
@@ -3,6 +3,7 @@ using CommunityApp.Services;
 using CommunityApp.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,42 @@ using System.Windows.Input;
 
 namespace CommunityApp.ViewModels
 {
-    public class HomePageViewModel:ViewModelBase
+    public class HomePageViewModel : ViewModelBase
     {
         private CommunityWebAPIProxy proxy;
         private IServiceProvider serviceProvider;
-        
+
         public HomePageViewModel(CommunityWebAPIProxy proxy, IServiceProvider serviceProvider)
         {
             this.serviceProvider = serviceProvider;
             this.proxy = proxy;
             CurrentCom = ((App)Application.Current).CurCom;
+            Reports = new ObservableCollection<Report>();
+            Notices = new ObservableCollection<Notice>();
         }
+        #region Declaring 
+        private ObservableCollection<Report> reports;
+        public ObservableCollection<Report> Reports
+        {
+            get => reports;
+            set
+            {
+                reports = value;
+                OnPropertyChanged(nameof(Reports));
+            }
+        }
+
+        private ObservableCollection<Notice> notices;
+        public ObservableCollection<Notice> Notices
+        {
+            get => notices;
+            set
+            {
+                notices = value;
+                OnPropertyChanged(nameof(Notices));
+            }
+        }
+        #endregion
         private Community? currentCom;
         public Community? CurrentCom
         {
