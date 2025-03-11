65359b45a9dbd2b3ee3ae9b6a76b83814b75b56a
RoeiDayan <roeidayan2007@gmail.com>
2025-03-11 04:15:44 +0200

    Working on join community

--- CommunityApp/ViewModels/JoinCommunityViewModel.cs
+++ CommunityApp/ViewModels/JoinCommunityViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CommunityApp.Models;
+using CommunityApp.Services;
+using CommunityApp.Views;
+using Microsoft.Maui.ApplicationModel.Communication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,94 @@ namespace CommunityApp.ViewModels
 {
     public class JoinCommunityViewModel:ViewModelBase
     {
+        private CommunityWebAPIProxy proxy;
+        private IServiceProvider serviceProvider;
+        public JoinCommunityViewModel(CommunityWebAPIProxy proxy, IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+            this.proxy = proxy;
+            JoinCommand = new Command(OnJoinCommunity);
+        }
+        #region Properties
+        private string communityCode;
+        public string CommunityCode
+        {
+            get => communityCode;
+            set
+            {
+                if (communityCode != value)
+                {
+                    communityCode = value;
+                    OnPropertyChanged(nameof(CommunityCode));
+                }
+            }
+        }
+
+        private int unitNum;
+        public int UnitNum
+        {
+            get => unitNum;
+            set
+            {
+                if (unitNum != value)
+                {
+                    unitNum = value; OnPropertyChanged(nameof(UnitNum));
+                }
+            }
+        }
+
+        private bool isLiable;
+        public bool IsLiable
+        {
+            get => isLiable;
+            set
+            {
+                isLiable = value; OnPropertyChanged(nameof(IsLiable));
+            }
+        }
+
+        private bool isResident;
+        public bool IsResident
+        {
+            get => isResident;
+            set
+            {
+                isResident = value; OnPropertyChanged(nameof (IsResident));
+            }
+        }
+
+        private bool isManager;
+        public bool IsManager
+        {
+            get => isManager;
+            set
+            {
+                isManager = value; OnPropertyChanged(nameof(IsManager));
+            }
+        }
+
+        private bool isProvider;
+        public bool IsProvider
+        {
+            get => isProvider;
+            set
+            {
+                isProvider = value; OnPropertyChanged(nameof(IsProvider));
+            }
+        }
+
+        #endregion
+
+        #region Commands
+        public Command JoinCommand { get;}
+        public Command LoginCommand { get;}
+        #endregion
+
+        #region Methods
+        private async void OnJoinCommunity()
+        {
+
+        }
+        #endregion
     }
 }
