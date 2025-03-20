1c65a0ebb2097ba49e57c839cce1791de2f4d469
RoeiDayan <roeidayan2007@gmail.com>
2025-03-20 03:14:41 +0200

    worked on create community

--- CommunityApp/ViewModels/StartCommunityViewModel.cs
+++ CommunityApp/ViewModels/StartCommunityViewModel.cs
@@ -21,9 +21,126 @@ namespace CommunityApp.ViewModels
         {
             this.proxy = proxy;
             this.serviceProvider = serviceProvider;
+            this.comId = 0;
+            this.userId = ((App)Application.Current).LoggedInUser.Id;
         }
 
         #region Properties
+        private int comId;
+
+        private int userId;
+
+        private string role;
+        public string Role
+        {
+            get => role;
+            set
+            {
+                if (role != value)
+                {
+                    role = value;
+                    OnPropertyChanged(nameof(Role));
+                }
+            }
+        }
+
+        private int balance;
+        public int Balance
+        {
+            get => balance;
+            set
+            {
+                if (balance != value)
+                {
+                    balance = value;
+                    OnPropertyChanged(nameof(Balance));
+                }
+            }
+        }
+
+        private int unitNum;
+        public int UnitNum
+        {
+            get => unitNum;
+            set
+            {
+                if (unitNum != value)
+                {
+                    unitNum = value;
+                    OnPropertyChanged(nameof(UnitNum));
+                }
+            }
+        }
+
+        private bool isLiable;
+        public bool IsLiable
+        {
+            get => isLiable;
+            set
+            {
+                if (isLiable != value)
+                {
+                    isLiable = value;
+                    OnPropertyChanged(nameof(IsLiable));
+                }
+            }
+        }
+
+        private bool isResident;
+        public bool IsResident
+        {
+            get => isResident;
+            set
+            {
+                if (isResident != value)
+                {
+                    isResident = value;
+                    OnPropertyChanged(nameof(IsResident));
+                }
+            }
+        }
+
+        private bool isManager;
+        public bool IsManager
+        {
+            get => isManager;
+            set
+            {
+                if (isManager != value)
+                {
+                    isManager = value;
+                    OnPropertyChanged(nameof(IsManager));
+                }
+            }
+        }
+
+        private bool isProvider;
+        public bool IsProvider
+        {
+            get => isProvider;
+            set
+            {
+                if (isProvider != value)
+                {
+                    isProvider = value;
+                    OnPropertyChanged(nameof(IsProvider));
+                }
+            }
+        }
+
+        private bool isApproved;
+        public bool IsApproved
+        {
+            get => isApproved;
+            set
+            {
+                if (isApproved != value)
+                {
+                    isApproved = value;
+                    OnPropertyChanged(nameof(IsApproved));
+                }
+            }
+        }
 
         #endregion
 
