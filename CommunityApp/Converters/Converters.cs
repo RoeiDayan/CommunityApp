b255f6b0e14b0ae55c76cde15ca5928c135b1900
RoeiDayan <roeidayan2007@gmail.com>
2025-04-21 00:18:09 +0300

    Added time to notices in home page

--- CommunityApp/Converters/Converters.cs
+++ CommunityApp/Converters/Converters.cs
@@ -16,7 +16,21 @@ namespace CommunityApp.Converters
             throw new NotImplementedException();
         }
     }
+    public class NullToBoolConverter : IValueConverter
+    {
+        public bool Invert { get; set; } = false;
 
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = value != null;
+            return Invert ? !result : result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
     public class BalanceColorConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
