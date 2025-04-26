d3dd9d4a2008b537e6190d0087d49856aea085ec
RoeiDayan <roeidayan2007@gmail.com>
2025-04-26 19:44:44 +0300

    Added expense

---
+++ CommunityApp/Models/Expense.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityApp.Models
+{
+    public class Expense
+    {
+        public int ExpenseId { get; set; }
+
+        public int ComId { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateOnly ExpenseDate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    
+    }
+}
